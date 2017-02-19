b53bfc8e3457e015a2db571501fc695d7a54b295
Fernando Balladares <balladares.fernando.92@gmail.com>
2017-02-19 17:23:30 -0500

    anoche

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-// request -> http:/localhost:8081/admin/
+// GET -> http:/localhost:8081/admin/
+
 router.get('/', function(req, res){
     var key = req.params.key;
     console.log(key)
     res.render('pages/admin', {'logged':true});
 });
 
-// request -> http:/localhost:8081/admin/asambleistas
+// GET -> http:/localhost:8081/admin/tabla  (Se solicita la lista de *)
+
 router.get('/:tablename', function(req, res){
     var tableName = req.params.tablename;
     var arr = [];
@@ -49,6 +51,9 @@ router.get('/:tablename', function(req, res){
     
 });
 
+// GET -> http:/localhost:8081/admin/tabla/id  
+//(Se solicita el formulario con la info de la fila id de una tabla.
+//en caso de pedirse la id = 0, se cargará el formulario vacío para poder ingresar un nuevo registro.
 
 router.get('/:tablename/:id', function(req, res){
     var tableName = req.params.tablename;
@@ -84,11 +89,9 @@ router.get('/:tablename/:id', function(req, res){
                     break;
                 default:
             }
-        
-        console.log("nueva id: " + newId)
-        if (rowId === '0'){
-            res.render('pages/CRUDinsert', {"tableName":tableName, "obj":obj, 'mode':'new', 'newId':newId});
-        }else res.render('pages/CRUDinsert', {"tableName":tableName, "obj":obj, 'mode':'edit'});
+            if (rowId === '0'){
+                res.render('pages/CRUDinsert', {"tableName":tableName, "obj":obj, 'mode':'new', 'newId':newId});
+            }else res.render('pages/CRUDinsert', {"tableName":tableName, "obj":obj, 'mode':'edit'});
             
 //            res.render('pages/CRUDinsert', {"tableName":tableName, "obj":arr[0]}, function(err, html){
 //                res.send(html);
@@ -97,6 +100,8 @@ router.get('/:tablename/:id', function(req, res){
     );
 });
 
+// POST -> http:/localhost:8081/admin/tabla/id
+//(Guarda los datos de un nuevo registro en la tabla, con dicha id )
 
 router.post('/:tablename/:id', function(req, res){
     var tableName = req.params.tablename;
@@ -106,8 +111,6 @@ router.post('/:tablename/:id', function(req, res){
     for (var attr in req.body){
         newObj[attr] = req.body[attr];
     }
-    
-    console.log(newObj)
     
     fs.readFile('data/exitpoll_prueba.json', 'utf8',
         function (err, texto) {
@@ -129,29 +132,102 @@ router.post('/:tablename/:id', function(req, res){
                     break;
                 default:
             }
-        fs.writeFile('data/exitpoll_prueba.json', JSON.stringify(objJSONFromFile, null, 3)); 
-        res.redirect('/admin/'+ tableName)
+            fs.writeFile('data/exitpoll_prueba.json', JSON.stringify(objJSONFromFile, null, 3), function(err, data){
+                if (err){
+                    console.log(err);
+                }
+            });  
+            res.redirect('/admin/'+ tableName)
+    });
+});
+
+// DELETE -> http:/localhost:8081/admin/tabla/id
+//(Borra los datos de un registro con dicha id, en la tabla)
+
+router.delete('/:tablename/:id', function(req, res){
+    var tableName = req.params.tablename;
+    var rowId = req.params.id;
+    
+    fs.readFile('data/exitpoll_prueba.json', 'utf8',
+        function (err, texto) {
+            var objJSONFromFile = JSON.parse(texto);
+        
+            switch (tableName){
+                case 'asambleistas':
+                case 'binomios':
+                case 'parlamentarios':
+                        objJSONFromFile.elecciones[0][tableName].splice(rowId -1 , 1);
+                    break;
+                case 'elecciones':
+                        obj = objJSONFromFile.elecciones.splice(rowId -1 , 1);
+                    break;
+                case 'usuarios':
+                        obj = objJSONFromFile.usuarios.splice(rowId -1 , 1);
+                    break;
+                case 'dignidades':
+                    break;
+                default:
+            }
+            fs.writeFile('data/exitpoll_prueba.json', JSON.stringify(objJSONFromFile, null, 3), function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send({deleted:false});
+                }else res.send({deleted:true});
+            });      
     });
 });
 
 
-//router.get('/:asambleistaaaas', function(req, res){
-// 
-//    var listaAsambleistas = [];
-//    
-//    fs.readFile("data/exitpoll.json", 'utf8',
-//        function (err, texto) {
-//            var objJSONFromFile = JSON.parse(texto);
-//            
-//    for (var d of objJSONFromFile.elecciones[0].asambleistas){
-//                        listaAsambleistas.push(d);
-//                    }
-//            res.render('pages/asambleistas', {"asambleistas":listaAsambleistas});
-////            res.render('pages/CRUDinsert', {"tableName":tableName, "obj":arr[0]}, function(err, html){
-////                res.send(html);
-////            });
-//        }
-//    );
-//});
+
+router.put('/:tablename/:id', function(req, res){
+    var tableName = req.params.tablename;
+    var rowId = req.params.id;
+    
+    var objEdited = req.body;
+    console.log("PIAL")
+    console.log(objEdited)
+    var response = {}
+    
+    fs.readFile('data/exitpoll_prueba.json', 'utf8',
+        function (err, texto) {
+            var objJSONFromFile = JSON.parse(texto);
+            var objAux = {};
+        
+            switch (tableName){
+                case 'asambleistas':
+                case 'binomios':
+                case 'parlamentarios':
+                    objAux = objJSONFromFile.elecciones[0][tableName][rowId - 1];
+                    break;
+                case 'elecciones':
+                    objAux = objJSONFromFile.elecciones[rowId - 1];
+                    break;
+                case 'usuarios':
+                    objAux = objJSONFromFile.usuarios[rowId - 1];
+                    break;
+                case 'dignidades':
+                    break;
+                default:
+            }
+            for (var key in objAux) {
+                console.log("Key: " + key)
+                if(objEdited[key]){
+                    objAux[key] = objEdited[key];
+                }
+            }
+            fs.writeFile('data/exitpoll_prueba.json', JSON.stringify(objJSONFromFile, null, 3), function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send({edited:false});
+                }else res.send({edited:true});
+            });  
+//            res.redirect('/admin/'+ tableName)
+        }
+    );
+});
+
+
+
+
 
 module.exports = router;
