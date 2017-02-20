be1b93d03ff816816e5d3580cdf0c658fb3f994a
Fernando Balladares <balladares.fernando.92@gmail.com>
2017-02-20 09:00:10 -0500

    gf

--- routes/index.js
+++ routes/index.js
@@ -10,37 +10,18 @@ router.use('/encue', require('./encue'));
 
 
 router.get('/', function(req, res){
-//    console.log('REQ = "/"')
-//    res.redirect('/login');
-    
-//    console.log('RENDER LOGIN')
     res.render('pages/login');
-    
-//    fs.readFile("data/exitpoll.json", 'utf8',
-//        function (err, texto) {
-//            var objJSONFromFile = JSON.parse(texto);
-//            
-//            for (var user of objJSONFromFile.usuarios){
-//                
-//            }
-//        }
-//    );
 });
 
 
-
-
-
-//router.get('/login', function(req, res){
-//    
-//});
-
-
 router.get('/logout', function(req, res){
-//    res.clearCookie('loggedUserId')
         res.clearCookie('loggedUserKey')
         res.redirect('/')
 });
 
+
+//router.get('*', function(req, res){
+//    res.redirect('/')
+//});
     
 module.exports = router;
